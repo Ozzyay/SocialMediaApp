c745a78fad313cd36223b75878edda8a35898a41
Ozzy Aytekin <ozzy.aytekin97@gmail.com>
2022-01-02 18:05:21 +1000

    CSS changes, title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,14 @@ import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
 import { Provider } from 'react-redux'
 import store from '../store/userStore'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <SessionProvider>
     <Provider store={store}>
+      <Head>
+        <title>OzSocial</title>
+      </Head>
     <Component {...pageProps} />
     </Provider>
     </SessionProvider>
